182d43ed5b7065d9c0156ab5f29749ed894a17b1
tonykjin <tonyjin23@gmail.com>
2019-07-28 20:11:13 +0000

    created initial graph read endpoint

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const server = app.listen(3000, err => {
 });
 
 const cred = require('./mysql_credentials');
-
 app.get('/entries', (req, res, next) => {
     const { date } = req.query;
     if (!date){
@@ -20,7 +19,7 @@ app.get('/entries', (req, res, next) => {
     }
     cred.connect(err => {
         if (err) throw err;
-        res.locals.con.query(`SELECT * FROM \`baby_entries\` AS b WHERE ${date} == b.date`, (err, result) => {
+        res.locals.con.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\` AS b WHERE ${date} == b.date`, (err, result) => {
             if (err) {
                 return next();
             }
@@ -30,6 +29,31 @@ app.get('/entries', (req, res, next) => {
         });
     });
 })
+
+
+app.get('/graph/:week', (req, res, next) => {
+    const now = new Date().now();
+    const weekAgo = now - 604800;
+    const datestampArr = {};
+    con.connect(err => {
+        if (err) throw err;
+        con.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\`WHERE finished_at BETwEEN FROM_UNIXTIME(${weekAgo}) AND FROM_UNIXTIME(${now})`, function(err, result, fields) {
+            if (err) {
+                return next();
+            }
+            result.forEach(element => {
+                let formatted = element["finished_at"].slice(11);
+                let date = new Date(formatted);
+                let day = date.getUTCDate();
+                let month = date.getUTCMonth();
+                datestampArr.push(month + '' + day);
+                })
+                datestampArr.sort((a,b) => {
+                    return a - b;
+                });
+            })
+    });
+});
 
 app.all('*', (err, req, res, next) => {
     res.sendStatus(500);
