4b60038fa7930be5d1573424b3315e1f8d9f0233
tonykjin <tonyjin23@gmail.com>
2019-07-28 19:56:10 +0000

    updated initial read-endpoint for error handling

--- server.js
+++ server.js
@@ -11,18 +11,26 @@ const server = app.listen(3000, err => {
 
 const cred = require('./mysql_credentials');
 
-app.get('/getDateData', (req, res) => {
-    const date = req.query.date;
+app.get('/entries', (req, res, next) => {
+    const { date } = req.query;
+    if (!date){
+        return res.status(422).send({
+            errors: ['No date provided'],
+        });
+    }
     cred.connect(err => {
         if (err) throw err;
         res.locals.con.query(`SELECT * FROM \`baby_entries\` AS b WHERE ${date} == b.date`, (err, result) => {
-            if (err) throw err;
-            res.send(JSON.stringify({
-                "status": 200, 
-                "error": null,
-                "response": result
+            if (err) {
+                return next();
+            }
+            res.status(200).send(JSON.stringify({
+                "entries": result
             }));
         });
     });
 })
 
+app.all('*', (err, req, res, next) => {
+    res.sendStatus(500);
+})
