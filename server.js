8a4d581e6ad7bf5385cd4f91482bf21b9a371904
tonykjin <tonyjin23@gmail.com>
2019-07-30 02:41:45 +0000

    fully functional graph/chart read endpoint. Data provided below

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.get('/entries', (req, res, next) => {
     }
     const startDate = date.concat(" 00:00:00");
     const endDate = date.concat(" 23:59:59");
-    console.log(startDate, endDate);
     connection.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\` WHERE finished_at BETWEEN "${startDate}" AND "${endDate}"`, (err, result) => {
         if (err) {
             return next(err);
@@ -39,38 +38,28 @@ app.get('/entries', (req, res, next) => {
 })
 
 const timeConvert = require('./time-convert.js');
-
-app.get('/graph/:week', (req, res, next) => {
+const formatDate = require('./format-date.js');
+app.get('/graph', (req, res, next) => {
     let current = Date.now();
-    const now = (timeConvert(current, 0)) + ' 23:59:59.999999';
-    const weekAgo = (timeConvert(current, 7)) + ' 00:00:00.000000';
+    const now = (timeConvert(current, 0)) + ' 23:59:59';
+    const weekAgo = (timeConvert(current, 7)) + ' 00:00:00';
     const feedingsArr = [0, 0, 0, 0, 0, 0, 0, 0];
     const changesArr = [0, 0, 0, 0, 0, 0, 0, 0];
     const napsArr = [0, 0, 0, 0, 0, 0, 0, 0];
-    con.connect(err => {
-        if (err) throw err; 
-        con.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\`WHERE finished_at BETwEEN ${weekAgo} AND ${now}`, function(err, result, fields) {
+    connection.connect(() => {
+        connection.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\`WHERE finished_at BETwEEN "${weekAgo}" AND "${now}"`, function(err, result, fields) {
             if (err) {
                 return next(err);
             }
+            console.log(result);
             result.forEach(data => {
-                let baseDate = now.slice(11);
-                let formatted = data["finished_at"].slice(11);
                 let entryType = data['entry_type'];
-                let date = new Date(formatted);
-                let day = date.getUTCDate();
-                let month = date.getUTCMonth();
-                let combinedDate = month + '' + day;
-                let i = 0; //position on given arr - starting from 7 days away from base
+                let baseDate = formatDate(Date.now());
+                let combinedDate = formatDate(data['finished_at']);
+                console.log(baseDate, combinedDate);
                 if (entryType) {
-                    let x = 7;
-                    if ((baseDate-combinedDate) > (x/2)) {
-                        x = 5;
-                        i = 3;
-                    } else if ((baseDate-combinedDate) < (x/2)) {
-                        x = 4;
-                        i = 0;
-                    }
+                    let x = 7; //difference check between baseDate and combinedDate
+                    let i = 0; //position on given arr - starting from 7 days away from base
                     while ( x >=0 ) {
                         if ((baseDate-combinedDate) === x) {
                             switch(entryType) {
@@ -87,7 +76,6 @@ app.get('/graph/:week', (req, res, next) => {
                         }
                     x--;
                     i++;
-                    continue;
                     }
                 }
             });
