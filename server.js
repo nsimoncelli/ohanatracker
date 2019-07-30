91bc98a8f0fc3e30ad3fa86288f731745937e395
tonykjin <tonyjin23@gmail.com>
2019-07-30 00:05:06 +0000

    fully functional calendar read endpoint, data shown below

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const server = app.listen(3000, err => {
+
+const server = app.listen(3001, err => {
     if (err) throw err;
-    console.log('started server on port 3000!');
+    console.log('started server on port 3001');
 });
 
 const cred = require('./mysql_credentials');
+const connection = mysql.createConnection(cred);
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected to database');
+});
+
 app.get('/entries', (req, res, next) => {
     const { date } = req.query;
     if (!date){
@@ -17,33 +25,33 @@ app.get('/entries', (req, res, next) => {
             errors: ['No date provided'],
         });
     }
-    const startDate = date + " 00:00:00.000000"
-    const endDate = date + " 23:59:59.999999"
-    cred.connect(err => {
-        if (err) throw err;
-        con.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\` AS b WHERE finished_at BETWEEN ${startDate} AND ${endDate}`, (err, result) => {
-            if (err) {
-                return next();
-            }
-            res.status(200).send(JSON.stringify({
-                "entries": result
-            }));
-        });
+    const startDate = date.concat(" 00:00:00");
+    const endDate = date.concat(" 23:59:59");
+    console.log(startDate, endDate);
+    connection.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\` WHERE finished_at BETWEEN "${startDate}" AND "${endDate}"`, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).send(JSON.stringify({
+            "entries": result
+        }));
     });
 })
 
-const timeConvert = require('./time-convert');
+const timeConvert = require('./time-convert.js');
+
 app.get('/graph/:week', (req, res, next) => {
-    const now = (timeConvert(new Date().now(), 0)) + ' 23:59:59.999999';
-    const weekAgo = (timeConvert(new Date().now(), 7)) + ' 00:00:00.000000';
+    let current = Date.now();
+    const now = (timeConvert(current, 0)) + ' 23:59:59.999999';
+    const weekAgo = (timeConvert(current, 7)) + ' 00:00:00.000000';
     const feedingsArr = [0, 0, 0, 0, 0, 0, 0, 0];
     const changesArr = [0, 0, 0, 0, 0, 0, 0, 0];
     const napsArr = [0, 0, 0, 0, 0, 0, 0, 0];
     con.connect(err => {
-        if (err) throw err;
+        if (err) throw err; 
         con.query(`SELECT id, entry_type, other_info, finished_at FROM \`baby_entries\`WHERE finished_at BETwEEN ${weekAgo} AND ${now}`, function(err, result, fields) {
             if (err) {
-                return next();
+                return next(err);
             }
             result.forEach(data => {
                 let baseDate = now.slice(11);
