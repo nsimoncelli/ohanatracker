2176640145621c64f3df6fb592ab7cf04878c43d
bmohabir1 <bisham.mohabir@gmail.com>
2019-07-30 23:18:26 -0700

    diapering button displays diapering options on click

--- client/components/logActionButtons.jsx
+++ client/components/logActionButtons.jsx
@@ -4,6 +4,7 @@ class LogActionButtons extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            view: 'main',
             show: false
         };
         this.showNotification = this.showNotification.bind(this);
@@ -11,75 +12,108 @@ class LogActionButtons extends React.Component {
         this.feedingClickHandler = this.feedingClickHandler.bind(this);
         this.nappingClickHandler = this.nappingClickHandler.bind(this);
         this.getCurrentTime = this.getCurrentTime.bind(this);
+        this.cancelDiapering = this.cancelDiapering.bind(this);
     }
 
     showNotification() {
-        this.setState({
-            show: true,
+        this.setState({ show: true,
         });
         setTimeout(() => {
              this.setState({
+                asleep: null,
                 show: false,
+                view: 'main'
             });
         }, 1500);
     }
 
     diaperClickHandler() {
-        this.showNotification();
-        console.log(this.getCurrentTime())
+        let diaperingView = "diapering";
+        this.setState({view : diaperingView})
     }
 
     feedingClickHandler() {
         this.showNotification();
-        console.log('clicked1')
+        console.log(this.getCurrentTime());
     }
 
     nappingClickHandler() {
         this.showNotification();
-        console.log('clicked2')
+        console.log(this.getCurrentTime());
+    }
+
+    cancelDiapering() {
+        let mainView = 'main';
+        this.setState({view : mainView})
     }
 
     getCurrentTime() {
         let today = new Date();
         let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
         let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        let dateTime = {
-            "date" : date,
-            "time" : time
-        };
+        let dateTime = `date: ${date} ${time}`;
         return dateTime;
     }
 
     render() {
-        return(
-            <div className="inputContainer row py-5 mt-4">
-                <div className="container">
-                    <div className="diapering row">
-                        <div className="diaperingButtonContainer col-12 text-center">
-                            <img src="/images/diaper.png" height="150px" width="auto" onClick={this.diaperClickHandler} />
+        if(this.state.view === 'main'){
+            return(
+                <div className="inputContainer row py-5 mt-4">
+                    <div className="container">
+                        <div className="diapering row">
+                            <div className="diaperingButtonContainer col-12 text-center">
+                                <img src="/images/diaper.png" height="150px" width="auto" onClick={this.diaperClickHandler} />
+                            </div>
+                        </div>
+                        <div className="feedingNapping row text-center">
+                            <div className="feedingButtonContainer col-6">
+                                <img src="/images/bottle2.png" height="150px" width="auto" onClick={this.feedingClickHandler} />
+                            </div>
+                            <div className="nappingButtonContainer col-6">
+                                <img src="/images/sleepingbaby3-b.png" height="150px" width="auto" onClick={this.nappingClickHandler} />    
+                            </div>
                         </div>
                     </div>
-                    <div className="feedingNapping row text-center">
-                        <div className="feedingButtonContainer col-6">
-                            <img src="/images/bottle2.png" height="150px" width="auto" onClick={this.feedingClickHandler} />
-                        </div>
-                        <div className="nappingButtonContainer col-6">
-                            <img src="/images/sleepingbaby3-b.png" height="150px" width="auto" onClick={this.nappingClickHandler} />    
-                        </div>
+                    <div className="row text-center my-5">
+                        <span className={this.state.show ? 'col-12' : 'hide'}>New baby entry has been made!</span>
                     </div>
                 </div>
-                <div className="row text-center my-5">
-                    <Notification show={this.state.show} />
+            )            
+        } else if (this.state.view === 'diapering') {
+            return (
+                <div className="inputContainer row py-5 mt-4">
+                    <div className="container">
+                        <div className="diapering row">
+                            <div className="diaperingButtonContainer col-12 text-center">
+                                <img src="/images/diaper.png" height="150px" width="auto" onClick={this.diaperClickHandler} />
+                            </div>
+                        </div>
+                        <div className="feedingNapping row text-center">
+                            <div className="feedingButtonContainer col-6">
+                                <img src="/images/pee3.png" height="150px" width="auto" onClick={this.feedingClickHandler} />
+                            </div>
+                            <div className="nappingButtonContainer col-6">
+                                <img src="/images/poop4.png" height="150px" width="auto" onClick={this.nappingClickHandler} />    
+                            </div>
+                        </div>
+                        <div className="cancelButton row my-3">
+                            <div className="cancelButtonContainer col-12 text-center">
+                                <img className={this.state.show ? 'hide' : ''} src="/images/x-button.png" height="60px" width="auto" onClick={this.cancelDiapering} />
+                                <img className={this.state.show ? '' : 'hide'} src="/images/check2.png" height="60px" width="auto" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row text-center my-5">
+                        <span className={this.state.show ? 'col-12' : 'hide'}>New baby entry has been made!</span>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
+    
+        
     }
 }
 
-class Notification extends React.Component {
-    render() {
-        return <span className={this.props.show ? 'show col-12' : 'col-12'}>New baby entry has been made!</span>
-    }
-}
 
 export default LogActionButtons;
+
