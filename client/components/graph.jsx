a7723f93dafb26267d5c68459be1edd5f118c506
Steve Min <stevekmin@gmail.com>
2019-07-31 23:23:20 +0000

    graph displays current days

--- client/components/graph.jsx
+++ client/components/graph.jsx
@@ -2,32 +2,52 @@ import React from 'react';
 import {Line} from 'react-chartjs-2';
 
 class Graph extends React.Component {
+    constructor(props) {
+        super(props);
+        this.createDateArray = this.createDateArray.bind(this);
+    }
+    createDateArray() {
+        let time = new Date().getTime();
+        let counter = 6;
+        let dateArray = [time];
+
+        for (let i = 1; i <= counter; i++){
+            let previousDay;
+            previousDay = time - (86400000*i);
+            dateArray.unshift(previousDay);
+        }
+
+        let dateStringArr = dateArray.map(day => {
+            let eachDay = new Date(day);
+            return (eachDay.toString().substring(0,11))
+        });
+
+        return dateStringArr;
+    }
     render() {
-        let today = new Date();
-        let date = (today.getMonth()+1)+'-'+ today.getDate() +'-'+ today.getFullYear();
         let data = {
-          labels: [date],
+          labels: this.createDateArray(),
           datasets: [{
             label: 'Feedings',
-            backgroundColor: 'rgb(255, 99, 132, .2)',
+            backgroundColor: 'rgb(1, 1, 132, .2)',
             borderColor: 'rgb(25, 100, 255, .8)',
             data: this.props.feedings
           }]
         };
         let data2 = {
-          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+          labels: this.createDateArray(),
           datasets: [{
             label: 'Changes',
-            backgroundColor: 'rgb(255, 99, 132, .2)',
+            backgroundColor: 'rgb(1, 1, 132, .2)',
             borderColor: 'rgb(25, 200, 150, .8)',
             data: this.props.changes
           }]
         };
         let data3 = {
-          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+          labels: this.createDateArray(),
           datasets: [{
             label: 'Naps',
-            backgroundColor: 'rgb(255, 99, 132, .4)',
+            backgroundColor: 'rgb(1, 99, 132, .4)',
             borderColor: 'rgb(230, 0, 150, .8)',
             data: this.props.naps
           }]
