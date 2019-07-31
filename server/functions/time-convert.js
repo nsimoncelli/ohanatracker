d7d39426d1f6faee651a54ce008fda921c64e53c
tonykjin <tonyjin23@gmail.com>
2019-07-31 00:41:45 +0000

    fixed graph ep to format data correctly

--- server/functions/time-convert.js
+++ server/functions/time-convert.js
@@ -1,7 +1,15 @@
 timeConvert = function(timestamp, change) {
-    const d = new Date(timestamp);
-        const converted = d.getFullYear() + '-0' + (d.getMonth()+1) + '-' + (d.getDate() - change) + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-        return converted;
+    if (timestamp === "now") {
+        let date = new Date();
+        let convertedDate = (date.getFullYear()) + '-' + (date.getMonth()+1) + '-' + (date.getDate() - change) + ' ';
+        let convertedTime = (date.getHours()) + ':' + (date.getMinutes()) + ':' + (date.getSeconds());
+        return convertedDate + convertedTime;
+    } else {
+        let d = new Date(timestamp);
+        let convertedDate = (d.getFullYear()) + '-' + (d.getMonth()+1) + '-' + (d.getDate() - change) + ' ';
+        let convertedTime = (d.getHours()) + ':' + (d.getMinutes()) + ':' + (d.getSeconds());
+        return convertedDate + convertedTime;
+    }
 }
 
 module.exports = timeConvert; 
