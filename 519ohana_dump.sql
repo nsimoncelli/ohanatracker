a9fa1d886a99177c14a7d5812d29e2d30833ca10
tonykjin <tonyjin23@gmail.com>
2019-08-02 18:09:57 +0000

    created new sql dump file

--- 519ohana_dump.sql
+++ 519ohana_dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 30, 2019 at 03:15 AM
+-- Generation Time: Aug 02, 2019 at 06:09 PM
 -- Server version: 5.7.26-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
@@ -50,8 +50,9 @@ CREATE TABLE `baby_entries` (
   `id` mediumint(9) NOT NULL,
   `baby_id` mediumint(9) NOT NULL,
   `user_id` mediumint(9) NOT NULL,
-  `started_at` datetime DEFAULT NULL,
-  `finished_at` datetime NOT NULL,
+  `started_at` timestamp NULL DEFAULT NULL,
+  `finished_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` date DEFAULT NULL,
   `entry_type` enum('feedings','naps','changes') COLLATE utf8_unicode_ci NOT NULL,
   `other_info` json NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -60,43 +61,64 @@ CREATE TABLE `baby_entries` (
 -- Dumping data for table `baby_entries`
 --
 
-INSERT INTO `baby_entries` (`id`, `baby_id`, `user_id`, `started_at`, `finished_at`, `entry_type`, `other_info`) VALUES
-(1, 1, 1, NULL, '2019-07-14 03:41:14', 'changes', '{\"change_type\": 2}'),
-(2, 1, 3, NULL, '2019-07-15 13:11:04', 'naps', '{}'),
-(3, 1, 2, NULL, '2019-07-14 03:41:14', 'changes', '{\"change_type\": 3}'),
-(4, 1, 2, NULL, '2019-07-15 13:14:54', 'feedings', '{}'),
-(5, 1, 2, NULL, '2019-07-15 13:14:54', 'feedings', '{}'),
-(6, 1, 2, NULL, '2019-07-14 03:41:14', 'changes', '{\"change_type\": 3}'),
-(9, 1, 1, NULL, '2019-07-29 14:20:00', 'naps', '{}'),
-(10, 1, 2, NULL, '2019-07-28 08:14:54', 'feedings', '{}'),
-(11, 1, 3, NULL, '2019-07-29 05:41:14', 'naps', '{}'),
-(12, 1, 2, NULL, '2019-07-27 09:23:44', 'changes', '{\"change_type\": 3}'),
-(13, 1, 1, NULL, '2019-07-27 19:25:46', 'changes', '{\"change_type\": 1}'),
-(14, 1, 3, NULL, '2019-07-28 23:37:59', 'changes', '{\"change_type\": 2}'),
-(15, 1, 2, NULL, '2019-07-28 10:41:28', 'feedings', '{}'),
-(16, 1, 1, NULL, '2019-07-26 07:37:28', 'feedings', '{}'),
-(17, 1, 3, NULL, '2019-07-23 12:30:29', 'naps', '{}'),
-(18, 1, 3, NULL, '2019-07-25 17:27:20', 'changes', '{\"change_type\": 3}'),
-(19, 1, 2, NULL, '2019-07-24 18:57:11', 'naps', '{}'),
-(20, 1, 1, NULL, '2019-07-24 14:18:08', 'feedings', '{}'),
-(21, 1, 2, NULL, '2019-07-24 18:22:23', 'changes', '{\"change_type\": 2}'),
-(22, 1, 1, NULL, '2019-07-24 09:11:27', 'naps', '{}'),
-(23, 1, 1, NULL, '2019-07-22 15:21:27', 'feedings', '{}'),
-(24, 1, 1, NULL, '2019-07-29 14:20:00', 'naps', '{}'),
-(25, 1, 2, NULL, '2019-07-28 08:14:54', 'feedings', '{}'),
-(26, 1, 3, NULL, '2019-07-29 05:41:14', 'naps', '{}'),
-(27, 1, 2, NULL, '2019-07-27 09:23:44', 'changes', '{\"change_type\": 3}'),
-(28, 1, 1, NULL, '2019-07-27 19:25:46', 'changes', '{\"change_type\": 1}'),
-(29, 1, 3, NULL, '2019-07-28 23:37:59', 'changes', '{\"change_type\": 2}'),
-(30, 1, 2, NULL, '2019-07-28 10:41:28', 'feedings', '{}'),
-(31, 1, 1, NULL, '2019-07-26 07:37:28', 'feedings', '{}'),
-(32, 1, 3, NULL, '2019-07-23 12:30:29', 'naps', '{}'),
-(33, 1, 3, NULL, '2019-07-25 17:27:20', 'changes', '{\"change_type\": 3}'),
-(34, 1, 2, NULL, '2019-07-24 18:57:11', 'naps', '{}'),
-(35, 1, 1, NULL, '2019-07-24 14:18:08', 'feedings', '{}'),
-(36, 1, 2, NULL, '2019-07-24 18:22:23', 'changes', '{\"change_type\": 2}'),
-(37, 1, 1, NULL, '2019-07-24 09:11:27', 'naps', '{}'),
-(38, 1, 1, NULL, '2019-07-22 15:21:27', 'feedings', '{}');
+INSERT INTO `baby_entries` (`id`, `baby_id`, `user_id`, `started_at`, `finished_at`, `date`, `entry_type`, `other_info`) VALUES
+(62, 1, 3, NULL, '2019-07-25 04:16:09', '2019-07-25', 'naps', '{}'),
+(63, 1, 2, NULL, '2019-07-25 06:20:18', '2019-07-25', 'feedings', '{}'),
+(64, 1, 2, NULL, '2019-07-25 08:34:28', '2019-07-25', 'changes', '{\"change_type\": 2}'),
+(65, 1, 1, NULL, '2019-07-25 13:40:29', '2019-07-25', 'naps', '{}'),
+(66, 1, 1, NULL, '2019-07-25 19:39:34', '2019-07-25', 'changes', '{\"change_type\": 1}'),
+(67, 1, 3, NULL, '2019-07-25 19:29:40', '2019-07-25', 'feedings', '{}'),
+(68, 1, 2, NULL, '2019-07-25 21:28:39', '2019-07-25', 'naps', '{}'),
+(69, 1, 3, NULL, '2019-07-26 07:36:35', '2019-07-26', 'feedings', '{}'),
+(70, 1, 2, NULL, '2019-07-26 08:32:33', '2019-07-26', 'naps', '{}'),
+(71, 1, 2, NULL, '2019-07-26 15:50:32', '2019-07-26', 'changes', '{\"change_type\": 3}'),
+(72, 1, 2, NULL, '2019-07-26 14:12:09', '2019-07-26', 'feedings', '{}'),
+(73, 1, 3, NULL, '2019-07-26 15:35:21', '2019-07-26', 'naps', '{}'),
+(74, 1, 2, NULL, '2019-07-27 11:43:31', '2019-07-27', 'changes', '{\"change_type\": 2}'),
+(75, 1, 2, NULL, '2019-07-27 11:43:31', '2019-07-27', 'changes', '{\"change_type\": 2}'),
+(76, 1, 1, NULL, '2019-07-28 11:43:31', '2019-07-28', 'changes', '{\"change_type\": 2}'),
+(78, 1, 3, NULL, '2019-07-30 11:43:31', '2019-07-30', 'changes', '{\"change_type\": 3}'),
+(79, 1, 1, NULL, '2019-07-31 11:43:31', '2019-07-31', 'changes', '{\"change_type\": 2}'),
+(80, 1, 3, NULL, '2019-07-28 11:43:31', '2019-07-28', 'changes', '{\"change_type\": 1}'),
+(82, 1, 1, NULL, '2019-07-30 18:43:31', '2019-07-30', 'changes', '{\"change_type\": 1}'),
+(83, 1, 3, NULL, '2019-07-31 11:43:31', '2019-07-31', 'changes', '{\"change_type\": 1}'),
+(84, 1, 1, NULL, '2019-07-30 01:43:31', '2019-07-30', 'changes', '{\"change_type\": 1}'),
+(85, 1, 3, NULL, '2019-08-01 12:43:31', '2019-08-01', 'changes', '{\"change_type\": 1}'),
+(86, 1, 3, NULL, '2019-08-01 06:43:31', '2019-08-01', 'changes', '{\"change_type\": 2}'),
+(87, 1, 2, NULL, '2019-08-01 07:43:31', '2019-08-01', 'changes', '{\"change_type\": 3}'),
+(88, 1, 2, NULL, '2019-07-27 03:14:00', '2019-07-27', 'naps', '{}'),
+(89, 1, 2, NULL, '2019-07-27 03:14:00', '2019-07-27', 'naps', '{}'),
+(90, 1, 3, NULL, '2019-07-27 03:14:00', '2019-07-27', 'naps', '{}'),
+(91, 1, 1, NULL, '2019-07-28 03:14:00', '2019-07-28', 'naps', '{}'),
+(92, 1, 2, NULL, '2019-07-28 13:14:00', '2019-07-28', 'naps', '{}'),
+(93, 1, 2, NULL, '2019-07-29 03:14:00', '2019-07-29', 'naps', '{}'),
+(94, 1, 1, NULL, '2019-07-29 15:14:00', '2019-07-29', 'naps', '{}'),
+(95, 1, 1, NULL, '2019-07-30 03:14:00', '2019-07-30', 'naps', '{}'),
+(96, 1, 2, NULL, '2019-07-31 03:14:00', '2019-07-31', 'naps', '{}'),
+(97, 1, 2, NULL, '2019-07-31 13:14:00', '2019-07-31', 'naps', '{}'),
+(98, 1, 3, NULL, '2019-07-31 22:14:00', '2019-07-31', 'naps', '{}'),
+(99, 1, 2, NULL, '2019-07-31 14:14:00', '2019-07-31', 'naps', '{}'),
+(100, 1, 3, NULL, '2019-08-01 03:14:00', '2019-08-01', 'naps', '{}'),
+(101, 1, 2, NULL, '2019-08-02 11:28:28', '2019-08-01', 'feedings', '{}'),
+(102, 1, 3, NULL, '2019-07-27 02:40:22', '2019-07-27', 'feedings', '{}'),
+(103, 1, 3, NULL, '2019-07-27 03:40:22', '2019-07-27', 'feedings', '{}'),
+(104, 1, 3, NULL, '2019-07-28 13:40:22', '2019-07-28', 'feedings', '{}'),
+(105, 1, 3, NULL, '2019-07-28 14:40:22', '2019-07-28', 'feedings', '{}'),
+(106, 1, 3, NULL, '2019-07-27 03:40:22', '2019-07-27', 'feedings', '{}'),
+(107, 1, 2, NULL, '2019-07-29 03:40:22', '2019-07-29', 'feedings', '{}'),
+(108, 1, 1, NULL, '2019-07-29 23:40:22', '2019-07-29', 'feedings', '{}'),
+(109, 1, 3, NULL, '2019-07-30 03:40:22', '2019-07-30', 'feedings', '{}'),
+(110, 1, 1, NULL, '2019-07-31 03:40:22', '2019-07-31', 'feedings', '{}'),
+(111, 1, 3, NULL, '2019-07-31 03:40:22', '2019-07-31', 'feedings', '{}'),
+(112, 1, 1, NULL, '2019-07-31 03:40:22', '2019-07-31', 'feedings', '{}'),
+(113, 1, 3, NULL, '2019-08-01 03:40:22', '2019-08-01', 'feedings', '{}'),
+(114, 1, 3, NULL, '2019-08-01 03:40:22', '2019-08-01', 'feedings', '{}'),
+(115, 1, 3, NULL, '2019-08-01 03:40:22', '2019-08-01', 'feedings', '{}'),
+(116, 1, 3, NULL, '2019-08-01 03:40:22', '2019-08-01', 'feedings', '{}'),
+(117, 1, 3, NULL, '2019-08-01 03:40:22', '2019-08-01', 'feedings', '{}'),
+(118, 1, 1, NULL, '2019-08-02 10:58:30', '2019-08-02', 'changes', '{\"change_type\": 1}'),
+(119, 1, 2, NULL, '2019-08-02 11:01:08', '2019-08-02', 'feedings', '{}'),
+(120, 1, 1, NULL, '2019-08-02 11:02:42', '2019-08-02', 'naps', '{}');
 
 -- --------------------------------------------------------
 
@@ -117,10 +139,7 @@ CREATE TABLE `roles` (
 INSERT INTO `roles` (`id`, `name`, `description`) VALUES
 (1, 'parent', 'dad'),
 (2, 'parent', 'mom'),
-(3, 'parent', 'dad'),
-(4, 'parent', 'mom'),
-(5, 'caretaker', 'daycare teacher'),
-(6, 'caretaker', 'daycare teacher');
+(3, 'caretaker', 'daycare teacher');
 
 -- --------------------------------------------------------
 
@@ -211,7 +230,7 @@ ALTER TABLE `babies`
 -- AUTO_INCREMENT for table `baby_entries`
 --
 ALTER TABLE `baby_entries`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
 --
 -- AUTO_INCREMENT for table `roles`
 --
