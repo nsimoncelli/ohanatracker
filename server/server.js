89fba141a74b7ec8cd7503b04ad4d73f28b6e26c
bmohabir1 <bisham.mohabir@gmail.com>
2019-08-04 12:51:37 -0700

    commented out cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-app.use(cors())
+// app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
